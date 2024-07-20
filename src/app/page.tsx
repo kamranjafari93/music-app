78c5cde782627fab5acd5eb0ed570ef45f4181ae
Kamran Jafari <kamran@roomvu.co>
2024-07-20 11:28:37 -0700

    Configure Linter and Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main className="">
-      Hello
-    </main>
-  );
+  return <main className="">Hello</main>;
 }
