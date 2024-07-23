9950e3bbca903f3b388bac6ab76c288f95666115
Kamran Jafari <kamran@roomvu.co>
2024-07-23 15:34:36 -0700

    Use dynamic loading for FavoriteArtistsList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import Header from "@/components/Header/Header";
-import FavoriteArtistsList from "@/components/FavoriteArtistsList/FavoriteArtistsList";
+import dynamic from "next/dynamic";
+
+const FavoriteArtistsList = dynamic(
+  () => import("@/components/FavoriteArtistsList/FavoriteArtistsList"),
+);
 
 export default function Home() {
   return (
